f605d22a5af8f56082c906de56505ea8576d7d53
Adam <anoah1225@gmail.com>
2019-07-16 11:03:06 -0400

    c# practice - first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class TestClass
+{
+    static void Main()
+    {
+        char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+
+string text = "one\ttwo three:four,five six seven";
+System.Console.WriteLine($"Original text: '{text}'");
+
+string[] words = text.Split(delimiterChars);
+System.Console.WriteLine($"{words.Length} words in text:");
+
+foreach (var word in words)
+{
+    System.Console.WriteLine($"<{word}>");
+}
+
+     }
+}
